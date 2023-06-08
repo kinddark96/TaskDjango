dd30e27b28292bc9241c49eee3ee584e8e50b9a6
dagon1196 <dagon1196@gmail.com>
2023-06-08 07:42:44 +0300

    step

--- myaccount/views.py
+++ myaccount/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
-from .models import *
 from .form import *
+from .models import *
 # Create your views here.
 def loginAcc(req):
     context={}
